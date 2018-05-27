de5a97c25546b9f6895bcf0587273331ef2f4fdd
Bartosz Ziółkowski <bartosz.k.ziolkowski@gmail.com>
2018-05-27 16:08:46 +0200

    edit i delete

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tasks.views import index, categories, add_category, edit_category, delete_category
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('categories/', categories, name='categories-list'),
+    path('add_category/', add_category, name='add_category'),
+    path('edit_category/<int:category_id>', edit_category, name='edit_category'),
+    path('delete_category/<int:category_id>', delete_category, name='delete_category')
 ]
+
+
